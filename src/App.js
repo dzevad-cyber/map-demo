c68ce84420a1657932f2ad6fa58049f623a1e09a
MansoN <dzevad.del@gmail.com>
2020-12-25 03:41:54 +0100

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
 import ReactMapGl from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -16,6 +17,7 @@ function App() {
       <ReactMapGl
         {...viewport}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={(viewport) => setViewport(viewport)}
       ></ReactMapGl>
     </div>
   );

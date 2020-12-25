f2281017e2c8216645d6db8b27b8d7f33caee144
MansoN <dzevad.del@gmail.com>
2020-12-25 16:15:05 +0100

    12th

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
         onViewportChange={(viewport) => setViewport(viewport)}
         mapStyle='mapbox://styles/mapbox/satellite-v9'
-        mapboxApiUrl='https://api.mapbox.com/mapbox-gl-js/v2.0.0/mapbox-gl.js?access_token=pk.eyJ1IjoicGF0aWVudDAiLCJhIjoiY2tqNGRwcXRiMGp1YjJ6bzllN3pvbHFpNCJ9.il1cJ3czzp4n0r8CrXsinw'
       >
         hello world
       </ReactMapGl>
